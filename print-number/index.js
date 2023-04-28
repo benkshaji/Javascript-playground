eeccae0dbe04fa11d94abbe2b158e3a928d74b5f
Ben K shaji <117567362+benkshaji@users.noreply.github.com>
2023-04-28 11:36:13 +0530

    print numbers from 1 to 100
    Javascript

---
+++ print-number/index.js
@@ -0,0 +1,16 @@
+for(let i=1; i<=100; i++){
+  // Print each number
+  console.log(i);
+}
+
+//! The above code is a simple for loop that will log numbers from 1 to 100 in the console.
+
+// The loop starts with the initialization statement let i=1, which sets the initial value of i to 1.
+
+// The condition i<=100 is the stopping condition of the loop. As long as this condition is true, the loop will continue to run.
+
+// The loop body contains a console.log(i) statement, which will log the current value of i to the console.
+
+// At the end of each iteration, the increment statement i++ is executed, which increases the value of i by 1.
+
+// So, the loop will run 100 times, logging the numbers from 1 to 100 in the console.
